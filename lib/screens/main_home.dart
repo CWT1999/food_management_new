974030af0d9eeb6b69fcff7033ae9af2c852dabe
CWT1999 <horuup@naver.com>
2022-08-05 19:44:55 +0900

    new commit123

--- lib/screens/main_home.dart
+++ lib/screens/main_home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_management/data/food.dart';
+import 'package:food_management/widget/level.dart';
+import 'package:provider/provider.dart';
 import 'main_food_menu.dart';
 
 
@@ -12,10 +14,15 @@ class MainHome extends StatefulWidget {
 }
 
 class _MainHomeState extends State<MainHome> {
+
+
   @override
   void initState() {
     super.initState();
   }
+
+  int _daycount = 0;
+
 
   @override
   Widget build(BuildContext context) {
@@ -30,11 +37,156 @@ class _MainHomeState extends State<MainHome> {
             title: Column(
               children: [
                 Text(
-                  "오토키트",
+                  "AutoKit",
                   style: TextStyle(
-                    fontFamily: 'RobotoSlab', fontSize: 25, color: Colors.black
+                      fontFamily: 'RobotoSlab',
+                      fontSize: 25,
+                      color: Colors.black
                   ),
                 )
+              ],
+            ),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.all(16.0),
+            sliver: SliverToBoxAdapter(
+              child: Row(
+                children: [
+                  Text("***님 반갑습니다",
+                    style: TextStyle(
+                        fontFamily: 'RobotoSlab',
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold
+                    ),),
+                ],
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    SizedBox(width: 30,),
+                    Text('D-day+$_daycount'),
+                    ElevatedButton(onPressed: () {
+                      setState(() { // setState() 추가.
+                        _daycount++;
+                      });
+                    }, style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)
+                        ),
+                        minimumSize: Size(20, 20)
+                    ),
+                        child: Icon(Icons.add)),
+
+                    ElevatedButton(onPressed: () {
+                      setState(() {
+                        if (_daycount != 0) { // setState() 추가.
+                          _daycount--;
+                        }
+                      });
+                    }, style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)
+                        ),
+                        minimumSize: Size(20, 20)
+                    ),
+                        child: Icon(Icons.remove)),
+                    SizedBox(width: 30,),
+                  ],
+
+                ),
+
+              ],
+            ),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.all(16.0),
+            sliver: SliverToBoxAdapter(
+              child: Row(
+                children: [
+                  Text("My Menu",
+                    style: TextStyle(
+                        fontFamily: 'RobotoSlab',
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold
+                    ),),
+                ],
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Spacer(),
+                    Text("아침 식단"),
+                    Spacer(),
+                    Text("순두부찌개"),
+                    Spacer(),
+                    InkWell(
+                      child: Icon(Icons.add),
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute<void>(
+                            fullscreenDialog: true,
+                            builder: (BuildContext context) {
+                              return MainFoodMenu();
+                            }));
+                        //Navigator.of(context).popUntil(ModalRoute.withName("/MainFoodMenu"));
+                      },
+                    ),
+                    Spacer()
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Spacer(),
+                    Text("점심 식단"),
+                    Spacer(),
+                    Text("김치우동"),
+                    Spacer(),
+                    InkWell(
+                      child: Icon(Icons.add),
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute<void>(
+                            fullscreenDialog: true,
+                            builder: (BuildContext context) {
+                              return MainFoodMenu();
+                            }));
+                        //Navigator.of(context).popUntil(ModalRoute.withName("/MainFoodMenu"));
+                      },
+                    ),
+                    Spacer()
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Spacer(),
+                    Text("저녁 식단"),
+                    Spacer(),
+                    Text("강된장찌개"),
+                    Spacer(),
+                    InkWell(
+                      child: Icon(Icons.add),
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute<void>(
+                            fullscreenDialog: true,
+                            builder: (BuildContext context) {
+                              return MainFoodMenu();
+                            }));
+                        //Navigator.of(context).popUntil(ModalRoute.withName("/MainFoodMenu"));
+                      },
+                    ),
+                    Spacer(),
+                  ],
+                ),
               ],
             ),
           ),
@@ -44,49 +196,63 @@ class _MainHomeState extends State<MainHome> {
               child: ImageCarousel(),
             ),
           ),
-          SliverPadding(
-            padding: EdgeInsets.all(16.0),
-            sliver: SliverToBoxAdapter(
-                child: Row(
-                      children: [
-                        Text("My Menu",
-                        style: TextStyle(
-                          fontFamily: 'RobotoSlab', fontSize: 25,fontWeight: FontWeight.bold
-                        ),),
-                  ],
-                ),
-              ),
-            ),
-          SliverToBoxAdapter(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(),
-                Text("아침 식단"),
-                Container(
-                  child: Text("순두부찌개"),
-                ),
-                InkWell(
-                  child: Icon(Icons.add),
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute<Null>(
-                        fullscreenDialog: true,
-                        builder:(BuildContext context){
-                          return MainFoodMenu();
-                        }));
-                    //Navigator.of(context).popUntil(ModalRoute.withName("/MainFoodMenu"));
-                       },
-                )
-              ],
-            ),
-          )
+          // SliverPadding(
+          //   padding: EdgeInsets.all(16.0),
+          //   sliver: SliverToBoxAdapter(
+          //     child: Row(
+          //       children: [
+          //         Count(),
+          //       ],
+          //     ),
+          //   ),
+          // ),
+
         ],
       ),
     );
   }
-
-
 }
+
+// Countn(int _daycount){
+//
+//     if(this._daycount>20){
+//       Text("브론즈");
+//     }
+//   }
+
+
+
+
+// class Count extends StatefulWidget {
+//   const Count({Key? key}) : super(key: key);
+//
+//   @override
+//   State<Count> createState() => _CountState();
+// }
+
+// class _CountState extends State<Count> {
+//   int _daycount=0;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: ElevatedButton(
+//           child: Text(
+//             '식단 시작 일수: d-day + $_daycount',
+//             style: TextStyle(
+//               fontFamily: 'RobotoSlab', fontSize: 25,fontWeight: FontWeight.bold
+//           ),),
+//           onPressed: () {
+//             setState(() { // setState() 추가.
+//               _daycount++;
+//             });
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 
 class IndicatorDot extends StatelessWidget {
   const IndicatorDot({Key? key, required this.isActive}) : super(key: key);
